46bcf9278d8ec5fef3ca9ddb284b4fc90a17021b
moskis <moskis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-14 21:58:23 +0000

    Tagging v2.2: * Updated to FancyBox 1.2.1 * Added new settings to Options Page: Easing, padding size, border color. * Tweaked CSS to prevent some themes from adding unwanted styles to FancyBox (especially background colors and link outlines). * Options Page reorganized in three sections: Appearance, Behavior and Troubleshooting Settings, to make settings easier to find.
    git-svn-id: https://plugins.svn.wordpress.org/fancybox-for-wordpress/tags/2.2@102195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fancybox.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Plugin Name: FancyBox for WordPress
+Plugin URI: http://blog.moskis.net/downloads/plugins/fancybox-for-wordpress/
+Description: Integrates <a href="http://fancy.klade.lv/">FancyBox</a> by <a href="http://klade.lv/">Janis Skarnelis</a> into WordPress. All images on a page are treated as a gallery allowing visitors to use Next and Previous buttons on the FancyBox frontend.
+Version: 2.2
+Author: Jose Pardilla
+Author URI: http://moskis.net/
+*/
+
+// If no options set, load defaults
+if (get_option('mfbfw_active_version') !== '2.2') {
+  update_option('mfbfw_active_version', '2.2');
+  add_option('mfbfw_autoApply', 'on');
+  add_option('mfbfw_borderColor', '#BBBBBB');
+  add_option('mfbfw_padding', '10');
+  add_option('mfbfw_imageScale', 'on');
+  add_option('mfbfw_zoomOpacity', 'on');
+  add_option('mfbfw_zoomSpeedIn', '500');
+  add_option('mfbfw_zoomSpeedOut', '500');
+  add_option('mfbfw_overlayShow', 'on');
+  add_option('mfbfw_overlayColor', '#666666');
+  add_option('mfbfw_overlayOpacity', '0.3');
+  add_option('mfbfw_easing', '');
+  add_option('mfbfw_easingIn', 'easeOutBack');
+  add_option('mfbfw_easingOut', 'easeInBack');
+  add_option('mfbfw_easingChange', 'easeInOutQuart');
+  add_option('mfbfw_hideOnContentClick', '');
+  add_option('mfbfw_centerOnScroll', 'on');
+  add_option('mfbfw_jQnoConflict', 'on');
+}
+
+
+function mfbfw_init() {
+
+  if (get_option('mfbfw_nojQuery') == 'on') { // default off
+
+    wp_enqueue_script('fancybox', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.fancybox-1.2.1.pack.js'); // Load fancybox without jQuery (for troubleshooting)
+
+  } else {
+
+    wp_enqueue_script('fancybox', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.fancybox-1.2.1.pack.js', array('jquery'), '1.3.2'); // Load fancybox with jQuery
+
+  }
+
+  if (get_option('mfbfw_easing') == 'on') {
+
+    wp_enqueue_script('jqueryeasing', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.easing.1.3.js', array('jquery'), '1.3.2'); // Load easing javascript file if required
+
+  }
+  
+}
+
+
+function mfbfw_css() {
+
+  echo "\n"."<!-- Fancybox for WordPress -->"."\n".'<link rel="stylesheet" href="'.WP_PLUGIN_URL.'/fancybox-for-wordpress/fancybox.css" type="text/css" media="screen" />'."\n";
+
+  ?>
+
+  <style type="text/css">
+    div#fancy_overlay {background-color:<?php echo get_option('mfbfw_overlayColor'); ?>}
+    div#fancy_inner {border-color:<?php echo get_option('mfbfw_borderColor'); ?>}
+  </style>
+
+  <?php
+  
+}
+
+
+function mfbfw_do() { ?>
+
+  <script type="text/javascript">
+
+  <?php if (get_option('mfbfw_jQnoConflict') == 'on') { ?> jQuery.noConflict(); <?php } ?>
+
+  jQuery(function(){
+
+    <?php if (get_option('mfbfw_autoApply') == 'on') { ?>
+
+      jQuery("a:has(img)[href$='.jpg']").attr({ rel: "fancybox" });  	// These five lines add the rel="fancybox" attribute to all links that contain
+      jQuery("a:has(img)[href$='.jpeg']").attr({ rel: "fancybox" });	// an image (i.e thumbnail) and link to a JPG, JPEG, PNG, GIF or BMP image.
+      jQuery("a:has(img)[href$='.gif']").attr({ rel: "fancybox" });
+      jQuery("a:has(img)[href$='.png']").attr({ rel: "fancybox" });
+      jQuery("a:has(img)[href$='.bmp']").attr({ rel: "fancybox" });
+
+    <?php } ?>
+
+    // This applies fancybox to the links we have just edited and applies the options set by the user on the admin panel.
+    jQuery("a[rel='fancybox']").fancybox({
+      'imageScale': <?php if (get_option('mfbfw_imageScale') == 'on') { echo "true"; } else { echo "false"; } ?>,
+      'padding': <?php echo get_option('mfbfw_padding'); ?>,
+      'zoomOpacity': <?php if (get_option('mfbfw_zoomOpacity') == 'on') { echo "true"; } else { echo "false"; } ?>,
+      'zoomSpeedIn': <?php echo get_option('mfbfw_zoomSpeedIn'); ?>,
+      'zoomSpeedOut': <?php echo get_option('mfbfw_zoomSpeedOut'); ?>,
+      'overlayShow': <?php if (get_option('mfbfw_overlayShow') == 'on') { echo "true"; } else { echo "false"; } ?>,
+      'overlayOpacity': <?php echo get_option('mfbfw_overlayOpacity'); ?>,
+      'hideOnContentClick': <?php if (get_option('mfbfw_hideOnContentClick') == 'on') { echo "true"; } else { echo "false"; } ?>,
+      'centerOnScroll': <?php if (get_option('mfbfw_centerOnScroll') == 'on') { echo "true"; } else { echo "false"; } ?><?php if (get_option('mfbfw_easing') == 'on') { ?>,
+      'easingIn': <?php echo '"' . get_option('mfbfw_easingIn') . '"'; ?>,
+      'easingOut': <?php echo '"' . get_option('mfbfw_easingOut') . '"'; ?>,
+      'easingChange': <?php echo '"' . get_option('mfbfw_easingChange') . '"'; ?>
+      <?php } ?>
+
+    });
+
+  });
+
+  </script>
+	
+  <?php echo "<!-- Fancybox for WordPress -->"."\n";
+
+}
+
+
+function mfbfw_admin() {
+
+  require dirname(__FILE__) . '/admin.php';
+  add_options_page('Fancybox for WordPress Options', 'Fancybox for WordPress', 10, __FILE__, 'mfbfw_options');
+  
+}
+
+
+
+// Start calling the functions
+add_action('wp_print_scripts', 'mfbfw_init'); // Add the fancybox script to the WordPress head
+add_action('wp_head', 'mfbfw_css');           // Load stylesheet
+add_action('wp_head', 'mfbfw_do');            // Apply fancyboy when the page loads
+add_action('admin_menu', 'mfbfw_admin');      // Admin Panel Page
+
+?>
