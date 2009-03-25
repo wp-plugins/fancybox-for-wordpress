33cae5b47941b783af602c286813e900c3247588
moskis <moskis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-25 17:38:37 +0000

    * Adding german MO file (a few strings missing) * Tagging v2.5
    git-svn-id: https://plugins.svn.wordpress.org/fancybox-for-wordpress/tags/2.5@105100 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fancybox.php
@@ -0,0 +1,464 @@
+<?php
+/*
+Plugin Name: FancyBox for WordPress
+Plugin URI: http://blog.moskis.net/downloads/plugins/fancybox-for-wordpress/
+Description: Integrates <a href="http://fancy.klade.lv/">FancyBox</a> by <a href="http://klade.lv/">Janis Skarnelis</a> into WordPress.
+Version: 2.5
+Author: Jose Pardilla
+Author URI: http://moskis.net/
+*/
+
+
+// When plugin is activated, update version, and set any new settings to default
+function mfbfw_install() {
+
+    update_option('mfbfw_active_version', '2.5');
+
+    add_option('mfbfw_borderColor', '#BBBBBB');
+    add_option('mfbfw_closeHorPos', 'right');
+    add_option('mfbfw_closeVerPos', 'top');
+    add_option('mfbfw_paddingColor', '#FFFFFF');
+    add_option('mfbfw_padding', '10');
+    add_option('mfbfw_overlayShow', 'on');
+    add_option('mfbfw_overlayColor', '#666666');
+    add_option('mfbfw_overlayOpacity', '0.3');
+    add_option('mfbfw_zoomOpacity', 'on');
+    add_option('mfbfw_zoomSpeedIn', '500');
+    add_option('mfbfw_zoomSpeedOut', '500');
+    add_option('mfbfw_easing', '');
+    add_option('mfbfw_easingIn', 'easeOutBack');
+    add_option('mfbfw_easingOut', 'easeInBack');
+    add_option('mfbfw_easingChange', 'easeInOutQuart');
+
+    add_option('mfbfw_imageScale', 'on');
+    add_option('mfbfw_centerOnScroll', 'on');
+    add_option('mfbfw_hideOnContentClick', '');
+    add_option('mfbfw_frameWidth', '640');
+    add_option('mfbfw_frameHeight', '500');
+
+    add_option('mfbfw_galleryType', 'all');
+    add_option('mfbfw_customExpression',
+'// This example expression will group all the image links in a page and make a gallery with them.
+// Each line adds support to a different image format: JPG, JPEG, GIF, PNG and BMP.
+
+jQuery("a:has(img)[href$=\'.jpg\']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+jQuery("a:has(img)[href$=\'.jpeg\']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+jQuery("a:has(img)[href$=\'.gif\']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+jQuery("a:has(img)[href$=\'.png\']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+jQuery("a:has(img)[href$=\'.bmp\']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();');
+
+    add_option('mfbfw_nojQuery', '');
+    add_option('mfbfw_jQnoConflict', 'on');
+
+    add_option('mfbfw_uninstall', '');
+
+    // Old settings, no longer used
+    delete_option('mfbfw_autoApply');
+    delete_option('mfbfw_closePosition');
+    delete_option('mfbfw_noTextLinks');
+  
+}
+
+
+// If requested, when plugin is deactivated, remove settings
+function mfbfw_uninstall() {
+
+  if (get_option('mfbfw_uninstall')) {
+
+    delete_option('mfbfw_active_version');
+
+    delete_option('mfbfw_borderColor');
+    delete_option('mfbfw_closeHorPos');
+    delete_option('mfbfw_closeVerPos');
+    delete_option('mfbfw_paddingColor');
+    delete_option('mfbfw_padding');
+    delete_option('mfbfw_overlayShow');
+    delete_option('mfbfw_overlayColor');
+    delete_option('mfbfw_overlayOpacity');
+    delete_option('mfbfw_zoomOpacity');
+    delete_option('mfbfw_zoomSpeedIn');
+    delete_option('mfbfw_zoomSpeedOut');
+    delete_option('mfbfw_easing');
+    delete_option('mfbfw_easingIn');
+    delete_option('mfbfw_easingOut');
+    delete_option('mfbfw_easingChange');
+
+    delete_option('mfbfw_imageScale');
+    delete_option('mfbfw_centerOnScroll');
+    delete_option('mfbfw_hideOnContentClick');
+    delete_option('mfbfw_frameWidth');
+    delete_option('mfbfw_frameHeight');
+
+    delete_option('mfbfw_autoApply');
+    delete_option('mfbfw_galleryType');
+    delete_option('mfbfw_customExpression');
+
+    delete_option('mfbfw_nojQuery');
+    delete_option('mfbfw_jQnoConflict');
+
+    delete_option('mfbfw_uninstall');
+
+  }
+
+}
+
+
+// Store plugin options in an array and return that array
+function mfbfw_get_settings() {
+
+  $mfbfwSettingsArray=array(
+
+    'version' => get_option('mfbfw_active_version'),
+
+    'borderColor' => get_option('mfbfw_borderColor'),
+    'closeHorPos' => get_option('mfbfw_closeHorPos'),
+    'closeVerPos' => get_option('mfbfw_closeVerPos'),
+    'paddingColor' => get_option('mfbfw_paddingColor'),
+    'padding' => get_option('mfbfw_padding'),
+    'overlayShow' => get_option('mfbfw_overlayShow'),
+    'overlayColor' => get_option('mfbfw_overlayColor'),
+    'overlayOpacity' => get_option('mfbfw_overlayOpacity'),
+    'zoomOpacity' => get_option('mfbfw_zoomOpacity'),
+    'zoomSpeedIn' => get_option('mfbfw_zoomSpeedIn'),
+    'zoomSpeedOut' => get_option('mfbfw_zoomSpeedOut'),
+    'easing' => get_option('mfbfw_easing'),
+    'easingIn' => get_option('mfbfw_easingIn'),
+    'easingOut' => get_option('mfbfw_easingOut'),
+    'easingChange' => get_option('mfbfw_easingChange'),
+
+    'imageScale' => get_option('mfbfw_imageScale'),
+    'centerOnScroll' => get_option('mfbfw_centerOnScroll'),
+    'hideOnContentClick' => get_option('mfbfw_hideOnContentClick'),
+    'frameWidth' => get_option('mfbfw_frameWidth'),
+    'frameHeight' => get_option('mfbfw_frameHeight'),
+
+    'galleryType' => get_option('mfbfw_galleryType'),
+    'customExpression' => get_option('mfbfw_customExpression'),
+
+    'nojQuery' => get_option('mfbfw_nojQuery'),
+    'jQnoConflict' => get_option('mfbfw_jQnoConflict'),
+
+    'uninstall' => get_option('mfbfw_uninstall')
+
+  );
+
+  return $mfbfwSettingsArray;
+
+}
+
+
+// Here we load FancyBox JS with jQuery and jQuery.easing if necessary
+function mfbfw_load() {
+
+  $settings = mfbfw_get_settings();
+
+  if (!is_admin()) { // avoid the scripts from loading on admin panel
+
+    if ($settings['nojQuery']) {
+
+      wp_enqueue_script('fancybox', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.fancybox-1.2.1.pack.js'); // Load fancybox without jQuery (for troubleshooting)
+
+    } else {
+
+      wp_enqueue_script('fancybox', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.fancybox-1.2.1.pack.js', array('jquery'), '1.3.2'); // Load fancybox with jQuery
+
+    }
+
+    if (get_option('mfbfw_easing')) {
+
+      wp_enqueue_script('jqueryeasing', WP_PLUGIN_URL . '/fancybox-for-wordpress/jquery.easing.1.3.js', array('jquery'), '1.3.2'); // Load easing javascript file if required
+
+    }
+
+  }
+  
+}
+
+
+// Link to FancyBox stylesheet and apply some custom styles
+function mfbfw_css() {
+
+  $settings = mfbfw_get_settings();
+
+  echo "\n"."<!-- Fancybox for WordPress v". $settings['version'] ." -->"."\n".'<link rel="stylesheet" href="'.WP_PLUGIN_URL.'/fancybox-for-wordpress/fancybox.css" type="text/css" media="screen" />'."\n";
+
+  ?>
+
+  <style type="text/css">
+    div#fancy_overlay {background-color:<?php echo $settings['overlayColor']; ?>}
+    div#fancy_inner {border-color:<?php echo $settings['borderColor']; ?>}
+    div#fancy_close {<?php echo $settings['closeHorPos']; ?>:-15px;<?php echo $settings['closeVerPos']; ?>:-12px;}
+    div#fancy_bg {background-color:<?php echo $settings['paddingColor']; ?>}
+  </style>
+
+  <?php
+  
+}
+
+
+// Load FancyBox with the settings set
+function mfbfw_init() {
+  
+  $settings = mfbfw_get_settings();
+  
+  ?>
+
+<script type="text/javascript">
+
+<?php if ($settings['jQnoConflict']) { ?>  jQuery.noConflict();<?php } echo "\n" ?>
+
+  jQuery(function(){
+
+   <?php // This copies the title of every IMG tag and add it to its parent A so that fancybox can use it ?>
+   jQuery.fn.getTitle = function() {
+     var arr = jQuery("a[rel^='fancybox']");
+     jQuery.each(arr, function() {
+       var title = jQuery(this).children("img").attr("title");
+       jQuery(this).attr('title',title);
+     })
+   }
+
+  <?php if ($settings['galleryType'] == 'post') {
+
+    // Gallery type BY POST and we are on post or page (so only one post or page is visible)
+    if ( is_single() | is_page() ) { ?>
+
+    jQuery("a:has(img)[href$='.jpg']").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.jpeg']").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.gif']").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.png']").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.bmp']").attr({ rel: "fancybox" }).getTitle();
+
+  <?php }
+
+  // Gallery type BY POST, but we are neither on post or page, so we make a different rel attribute on each post
+  else { ?>
+
+    jQuery('.post:eq(0)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox1').getTitle();
+    jQuery('.post:eq(1)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox2').getTitle();
+    jQuery('.post:eq(2)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox3').getTitle();
+    jQuery('.post:eq(3)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox4').getTitle();
+    jQuery('.post:eq(4)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox5').getTitle();
+    jQuery('.post:eq(5)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox6').getTitle();
+    jQuery('.post:eq(6)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox7').getTitle();
+    jQuery('.post:eq(7)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox8').getTitle();
+    jQuery('.post:eq(8)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox9').getTitle();
+    jQuery('.post:eq(9)').contents().find("a:has(img)[href$='.jpg']").attr('rel','fancybox10').getTitle();
+
+    jQuery('.post:eq(0)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox1').getTitle();
+    jQuery('.post:eq(1)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox2').getTitle();
+    jQuery('.post:eq(2)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox3').getTitle();
+    jQuery('.post:eq(3)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox4').getTitle();
+    jQuery('.post:eq(4)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox5').getTitle();
+    jQuery('.post:eq(5)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox6').getTitle();
+    jQuery('.post:eq(6)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox7').getTitle();
+    jQuery('.post:eq(7)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox8').getTitle();
+    jQuery('.post:eq(8)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox9').getTitle();
+    jQuery('.post:eq(9)').contents().find("a:has(img)[href$='.jpeg']").attr('rel','fancybox10').getTitle();
+
+    jQuery('.post:eq(0)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox1').getTitle();
+    jQuery('.post:eq(1)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox2').getTitle();
+    jQuery('.post:eq(2)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox3').getTitle();
+    jQuery('.post:eq(3)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox4').getTitle();
+    jQuery('.post:eq(4)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox5').getTitle();
+    jQuery('.post:eq(5)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox6').getTitle();
+    jQuery('.post:eq(6)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox7').getTitle();
+    jQuery('.post:eq(7)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox8').getTitle();
+    jQuery('.post:eq(8)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox9').getTitle();
+    jQuery('.post:eq(9)').contents().find("a:has(img)[href$='.gif']").attr('rel','fancybox10').getTitle();
+
+    jQuery('.post:eq(0)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox1').getTitle();
+    jQuery('.post:eq(1)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox2').getTitle();
+    jQuery('.post:eq(2)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox3').getTitle();
+    jQuery('.post:eq(3)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox4').getTitle();
+    jQuery('.post:eq(4)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox5').getTitle();
+    jQuery('.post:eq(5)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox6').getTitle();
+    jQuery('.post:eq(6)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox7').getTitle();
+    jQuery('.post:eq(7)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox8').getTitle();
+    jQuery('.post:eq(8)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox9').getTitle();
+    jQuery('.post:eq(9)').contents().find("a:has(img)[href$='.png']").attr('rel','fancybox10').getTitle();
+
+    jQuery('.post:eq(0)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox1').getTitle();
+    jQuery('.post:eq(1)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox2').getTitle();
+    jQuery('.post:eq(2)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox3').getTitle();
+    jQuery('.post:eq(3)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox4').getTitle();
+    jQuery('.post:eq(4)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox5').getTitle();
+    jQuery('.post:eq(5)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox6').getTitle();
+    jQuery('.post:eq(6)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox7').getTitle();
+    jQuery('.post:eq(7)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox8').getTitle();
+    jQuery('.post:eq(8)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox9').getTitle();
+    jQuery('.post:eq(9)').contents().find("a:has(img)[href$='.bmp']").attr('rel','fancybox10').getTitle();
+
+  <?php }
+  
+  }
+
+  // Gallery type ALL
+  elseif ($settings['galleryType'] == 'all') { ?>
+
+    jQuery("a:has(img)[href$='.jpg']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.jpeg']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.gif']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.png']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+    jQuery("a:has(img)[href$='.bmp']").addClass("fancybox").attr({ rel: "fancybox" }).getTitle();
+
+  <?php }
+
+  // Gallery type NONE
+  elseif ($settings['galleryType'] == 'none') { ?>
+
+    jQuery("a:has(img)[href$='.jpg']").addClass("fancybox").getTitle();
+    jQuery("a:has(img)[href$='.jpeg']").addClass("fancybox").getTitle();
+    jQuery("a:has(img)[href$='.gif']").addClass("fancybox").getTitle();
+    jQuery("a:has(img)[href$='.png']").addClass("fancybox").getTitle();
+    jQuery("a:has(img)[href$='.bmp']").addClass("fancybox").getTitle();
+
+  <?php }
+
+  // Else, gallery type is custom, so we just print the custom expression
+  else {
+
+    echo $settings['customExpression'];
+
+  }
+  
+  // Now we call fancybox and apply it on any link with a rel atribute that starts with "fancybox", with the options set on the admin panel ?>
+  jQuery("a.fancybox").fancybox({
+    'imageScale': <?php if ($settings['imageScale']) { echo "true"; } else { echo "false"; } ?>,
+    'padding': <?php echo $settings['padding']; ?>,
+    'zoomOpacity': <?php if ($settings['zoomOpacity']) { echo "true"; } else { echo "false"; } ?>,
+    'zoomSpeedIn': <?php echo $settings['zoomSpeedIn']; ?>,
+    'zoomSpeedOut': <?php echo $settings['zoomSpeedOut']; ?>,
+    'overlayShow': <?php if ($settings['overlayShow']) { echo "true"; } else { echo "false"; } ?>,
+    'overlayOpacity': <?php echo $settings['overlayOpacity']; ?>,
+    'hideOnContentClick': <?php if ($settings['hideOnContentClick']) { echo "true"; } else { echo "false"; } ?>,
+    'centerOnScroll': <?php if ($settings['centerOnScroll']) { echo "true"; } else { echo "false"; } ?><?php if ($settings['easing']) { ?>,
+    'easingIn': <?php echo '"' . $settings['easingIn'] . '"'; ?>,
+    'easingOut': <?php echo '"' . $settings['easingOut'] . '"'; ?>,
+    'easingChange': <?php echo '"' . $settings['easingChange'] . '"'; ?>
+    <?php } ?>
+
+  });
+
+})
+
+</script>
+<?php echo "<!-- END Fancybox for WordPress -->"."\n";
+
+}
+
+
+// Admin Head
+function mfbfw_admin_head() {
+
+  ?>
+
+  <script type="text/javascript">
+
+    jQuery(function(){
+
+      // Advanced Settings Switcher
+      jQuery("#advOpsSwitch").click(function(){
+        jQuery(".advOpts").toggle("slow");
+      });
+
+      // Troubleshooting & Settings Switcher
+      jQuery("#troOpsSwitch").click(function(){
+        jQuery(".troOpts").toggle("slow");
+      });
+
+      // Hide Custom Expresion textarea if not needed
+      var galleryType = jQuery("input:radio[name=mfbfw_galleryType]:checked").val();
+
+      if (galleryType == "all") {
+        jQuery("#customExpressionBlock").css("display", "none");
+      } else if (galleryType == "post") {
+        jQuery("#customExpressionBlock").css("display", "none");
+      }
+
+      jQuery("#mfbfw_galleryTypeAll").click(function () {
+        jQuery("#customExpressionBlock").hide("slow");
+      });
+
+      jQuery("#mfbfw_galleryTypePost").click(function () {
+        jQuery("#customExpressionBlock").hide("slow");
+      });
+
+      jQuery("#mfbfw_galleryTypeCustom").click(function () {
+        jQuery("#customExpressionBlock").show("slow");
+      });
+
+    })
+
+  </script>
+
+  <?php
+
+}
+
+
+// Admin options page
+function mfbfw_admin_menu() {
+
+  require dirname(__FILE__) . '/admin.php';
+
+//  global $wp_version;
+//  $menutitle = '';
+//
+//  if ( version_compare( $wp_version, '2.6.999', '>' ) ) {
+//
+//    $menutitle = '<img src="' . WP_PLUGIN_URL .'/fancybox-for-wordpress/img/extra_menu.png" alt="" width="12" height="12" />' . ' ';
+//    $menutitle .= ('Fancybox for WP');
+//    add_options_page('Fancybox for WordPress Options', $menutitle, 10, 'fancybox-for-wordpress', 'mfbfw_options_page');
+//  } else {
+    add_submenu_page('options-general.php', 'Fancybox for WordPress Options', 'Fancybox for WP', 10, 'fancybox-for-wordpress', 'mfbfw_options_page');
+//  }
+
+}
+
+
+// Load text domain
+function mfbfw_textdomain() {
+
+  if (function_exists('load_plugin_textdomain')) {
+    load_plugin_textdomain('mfbfw', 'wp-content/plugins/fancybox-for-wordpress/languages', 'fancybox-for-wordpress/languages');
+  }
+
+}
+
+
+// Settings Button on Plugins Panel
+function mfbfw_plugin_action_links($links, $file) {
+
+	static $this_plugin;
+	if ( ! $this_plugin ) $this_plugin = plugin_basename(__FILE__);
+
+	if ( $file == $this_plugin ){
+		$settings_link = '<a href="options-general.php?page=fancybox-for-wordpress">' . __('Settings', 'mfbfw') . '</a>';
+		array_unshift( $links, $settings_link );
+	}
+
+	return $links;
+
+}
+
+
+
+// Actions
+add_action('init', 'mfbfw_textdomain');           // Load Text Domain
+add_action('wp_print_scripts', 'mfbfw_load');     // Load FancyBox Script
+add_action('wp_head', 'mfbfw_css');               // Fancybox Stylesheet
+add_action('wp_head', 'mfbfw_init');              // Initiate Fancybox
+add_action('admin_head', 'mfbfw_admin_head');     // Admin Panel Head
+add_action('admin_menu', 'mfbfw_admin_menu');     // Admin Panel Page
+
+
+// Filters
+add_filter( 'plugin_action_links', 'mfbfw_plugin_action_links', 10, 2 ); // Settings Button on Plugins Panel
+
+
+// Install and Uninstall
+register_activation_hook(__FILE__,'mfbfw_install');
+register_deactivation_hook(__FILE__,'mfbfw_uninstall');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,145 @@
+=== FancyBox for WordPress ===
+Contributors: moskis
+Donate link: https://www.asmallorange.com/extras/donate/?id=10218
+Tags: fancybox, lightbox, jquery, image, gallery
+Requires at least: 2.7
+Tested up to: 2.7.1
+Stable tag: 2.5
+
+Seamlessly integrates FancyBox into your blog: Upload, activate, and you're done. No further configuration needed. However, you can customize it from the Options Page if you like... :)
+
+
+== Description ==
+
+Seamlessly integrates FancyBox into your blog: Upload, activate, and you're done. No further configuration needed. However, you can customize it from the Options Page if you like... :)
+
+You can easely customize almost anything you can think about fancybox: the border, margin width and color, zoom speed, animation type, close button position, overlay color and opacity and even more advanced option like several options to group images into galleries, and more...
+
+By default, the plugin will use jQuery to apply [FancyBox](http://fancy.klade.lv/) to ANY image links that open an image. This includes posts, the sidebar, etc, so you can activate it and it will be applied automatically.
+
+Finally, i have only tested the plugin in WordPress 2.7, so it's very recomended to use it with WordPress 2.7 or later. After all, there's no reason why you shouldn't be already using WP2.7, is there? ;)
+
+
+= Changelog =
+
+2.5 Updates:
+
+* Support for localizations (Spanish and German localizations included)
+* Some parts of the code completely rewritten
+* Fixed fancybox files being loaded on the admin pages
+* New options for close button position, custom jquery expressions, iframe content
+* Options page mostly rewritten, better organized.
+* Medium/advanced, troubleshooting/uninstall options collapsable, hidden by default
+* Better support guidelines and links on options page
+* Settings link on the Manage plugins page
+* Custom expression hidden when not used
+* Title atribute on IMG tags is now copied to its parent A tag for better caption support
+* New uninstall options and better handling of new options when installing/updating
+* Cleans any old options no longer needed when plugin is activated/updated
+
+
+2.2 Updtades:
+
+* Updated to FancyBox 1.2.1
+* Added new settings to Options Page: Easing, padding size, border color.
+* Tweaked CSS to prevent some themes from adding unwanted styles to fancybox (especially background colors and link outlines).
+* Options Page reorganized in three sections: Appearance, Behaviour and Troubleshooting Settings, to make settings easier to find.
+
+
+2.1.1 Updtades:
+
+* Fixed a new bug introduced in 2.1 that prevented options from being saved. Sorry about the mess :(
+
+
+2.1 Updtades:
+
+* Fixed a major bug in 2.0 that prevented it from working until plugin's options page was visited.
+* Added two options for troubleshooting that might help in some cases if the plugin doesn't work: disable jQuery noConflict and skip jQuery call.
+* Additional fixes to caption CSS: Captions should look better now in Hybrid theme, child themes, and other situations where general table elements are improperly styled.
+
+
+2.0 Updates:
+
+* Brand new Options Page in Admin Panel lets you easely customize many options: fancybox auto apply, image resize to fit, opacity fade while zooming, zoom speed, overlay on/off, overlay color, overlay opacity, close fancybox on image click, keep fancybox centered while scrolling.
+* CSS completely updated for FancyBox 1.2.0
+* Captions fixed in IE
+
+
+1.3 Updates:
+
+* Shadows and Close button should be fixed now
+
+
+1.2 Updates:
+
+* Updated to FancyBox 1.2.0
+* Uses packed version of the JavaScript file (8kb instead of 14kb)
+
+
+1.1 Updates:
+
+* Fixed FancyBox not being applied to .jpeg files
+* Fixed "Click to close" overlay text
+* Moved images to /img/ folder
+
+
+= Known Bugs =
+
+* Shadows not perfect yet on IE6/7, but close
+
+
+== Installation ==
+
+1. Upload the `fancybox-for-wordpress` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. That's it, [FancyBox](http://fancy.klade.lv/) will be automatically applied to all your image links and galleries.
+4. If you want to customize a bit the look and feel of FancyBox, go to the Options Page under General Options in the WordPress Admin panel.
+
+
+== Screenshots ==
+
+1. Simple example of fancybox on a post. [Live demo here](http://blog.moskis.net/downloads/plugins/fancybox-for-wordpress/).
+2. Basic settings on Options Page in the Admin Panel. This makes it very easy to customize the plugin to your needs.
+2. Full settings on Options Page.
+
+
+== Frequently Asked Questions ==
+
+= Are you the author of FancyBox? =
+
+NO. I just ported it to WordPress. For more info on the FancyBox script itself [visit its website](http://fancy.klade.lv/home).
+
+= Help translate the plugin to your language =
+
+If you want to make a localization you can use the sources in the laguages folder and email me the PO and MO files (or just the PO) at jose (at) moskis.net. I will add it in the next release and if you want me to, i'll send you updated pot file a couple of days before future releases.
+
+= The plugin does not work =
+
+The most common problem is another plugin or your theme conflicting with the plugin. This is usually caused by some plugin that load jQuery directly instead of using the wp_enqueue_script function. Try following all the guidelines in the options page, and if necesary try the troubleshooting settings.
+
+If the plugin is not working or you find any bug/bad behaviour/conflict deactivate it and email me at jose (at) moskis.net with a description of the problem, and i'll take a look at it.
+
+If activating the plugin somehow brakes your panel (cant imagine that happening but just in case) delete the plugin from the plugins folder and go to your blog panel.
+
+= Suggestions and feature requests? =
+
+Don't hesitate to email me at jose (at) moskis.net with any thoughts about this plugin, feature requests, issues, doubts suggestions, anything goes. :)
+
+= How does the plugin exactly work? =
+
+First the plugin checks if your blog is using jQuery, if not it will load it from the WordPress files. Then it will load the FancyBox JavaScript, which is where all the magic happens. After this all the CSS code needed to beautify the plugin is added to the page. Finally, the JavaScript wait for your blog to finish loading and then it looks for all links where FancyBox can be applied, and adds it. And that's it. :)
+
+= The plugin does not work with Mandingo theme =
+
+To fix this edit the file /themes/mandigo/header.php and remove the line 231:
+
+<code>&lt;script type="text/javascript" src="&lt;?php echo $dirs['www']['js']; ?&gt;jquery.js"&gt;&lt;/script&gt;</code>
+
+After applying this fix everything seems to work perfectly.
+
+
+== Demo ==
+
+You can see the plugin working on [my blog](http://blog.moskis.net/downloads/plugins/fancybox-for-wordpress/) although there's nothing amazing to see, just a FancyBox simple implementation, that's the point ;)
+
+You can take a look at the code if you're curious, though. You will find it in the Head section of the page.
