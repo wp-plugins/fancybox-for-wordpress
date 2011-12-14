84124c06e510fceab619b5ecfa3c867a02976e5a
moskis <moskis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-14 01:53:52 +0000

    tagging 3.0.0
    git-svn-id: https://plugins.svn.wordpress.org/fancybox-for-wordpress/tags/3.0.0@475125 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ settings.php
@@ -0,0 +1,141 @@
+<?php
+
+/*-----------------------------------------------------------------------------------*/
+/* Settings setup
+/*-----------------------------------------------------------------------------------*/
+
+
+// If lower than 3.0.0 we need to get old options, convert, and delete them
+if ( get_option( 'mfbfw_padding' ) !== false ) {
+	
+	$old_settings_array = array (
+
+		'titleShow'							=> get_option('mfbfw_showTitle'),
+		'border'								=> get_option('mfbfw_border'),
+		'borderColor'						=> get_option('mfbfw_borderColor'),
+		'closeHorPos'						=> get_option('mfbfw_closeHorPos'),
+		'closeVerPos'						=> get_option('mfbfw_closeVerPos'),
+		'paddingColor'					=> get_option('mfbfw_paddingColor'),
+		'padding'								=> get_option('mfbfw_padding'),
+		'overlayShow'						=> get_option('mfbfw_overlayShow'),
+		'overlayColor'					=> get_option('mfbfw_overlayColor'),
+		'overlayOpacity'				=> get_option('mfbfw_overlayOpacity'),
+		'zoomOpacity'						=> get_option('mfbfw_zoomOpacity'),
+		'zoomSpeedIn'						=> get_option('mfbfw_zoomSpeedIn'),
+		'zoomSpeedOut'					=> get_option('mfbfw_zoomSpeedOut'),
+		'zoomSpeedChange'				=> get_option('mfbfw_zoomSpeedChange'),
+		'easing'								=> get_option('mfbfw_easing'),
+		'easingIn'							=> get_option('mfbfw_easingIn'),
+		'easingOut'							=> get_option('mfbfw_easingOut'),
+		'easingChange'					=> get_option('mfbfw_easingChange'),
+
+		'imageScale'						=> get_option('mfbfw_imageScale'),
+		'enableEscapeButton'		=> get_option('mfbfw_enableEscapeButton'),
+		'showCloseButton'				=> get_option('mfbfw_showCloseButton'),
+		'centerOnScroll'				=> get_option('mfbfw_centerOnScroll'),
+		'hideOnOverlayClick'		=> get_option('mfbfw_hideOnOverlayClick'),
+		'hideOnContentClick'		=> get_option('mfbfw_hideOnContentClick'),
+		'loadAtFooter'					=> get_option('mfbfw_loadAtFooter'),
+		'frameWidth'						=> get_option('mfbfw_frameWidth'),
+		'frameHeight'						=> get_option('mfbfw_frameHeight'),
+		
+		'callbackOnStart'				=> get_option('mfbfw_callbackOnStart'),
+		'callbackOnComplete'		=> get_option('mfbfw_callbackOnShow'),
+		'callbackOnClose'				=> get_option('mfbfw_callbackOnClose'),
+
+		'galleryType'						=> get_option('mfbfw_galleryType'),
+		'customExpression'			=> get_option('mfbfw_customExpression'),
+
+		'nojQuery'							=> get_option('mfbfw_nojQuery'),
+		'jQnoConflict'					=> get_option('mfbfw_jQnoConflict'),
+
+		'uninstall'							=> get_option('mfbfw_uninstall'),
+		
+		
+		// New Settings since 3.0
+		'titlePosition'					=> 'inside',// outside no tiene maquetacion css?
+		'titleColor'						=> '#333333',// !!
+		'showNavArrows'					=> 'on',//
+		
+		'transitionIn'					=> 'fade',
+		'transitionOut'					=> 'fade',
+		
+		'cyclic'								=> '',//
+		'mouseWheel'						=> '',// !!!
+		
+		'autoDimensions'				=> 'on',//
+		
+		'callbackEnable'				=> '', //
+		'callbackOnCancel'			=> 'function() { alert("Cancel!"); }',//
+		'callbackOnCleanup'			=> 'function() { alert("CleanUp!"); }',//
+		
+		'extraCallsEnable'			=> '',//
+		'extraCalls'						=> '',//
+
+	);
+	
+	// save old settings to database in a single serialized option
+	add_option( 'mfbfw', $old_settings_array );
+	
+	// Get deprecated settings and delete them from database
+	$deprecated_array = array (
+		'mfbfw_version',
+		'mfbfw_showTitle',
+		'mfbfw_border',
+		'mfbfw_borderColor',
+		'mfbfw_closeHorPos',
+		'mfbfw_closeVerPos',
+		'mfbfw_paddingColor',
+		'mfbfw_padding',
+		'mfbfw_overlayShow',
+		'mfbfw_overlayColor',
+		'mfbfw_overlayOpacity',
+		'mfbfw_zoomOpacity',
+		'mfbfw_zoomSpeedIn',
+		'mfbfw_zoomSpeedOut',
+		'mfbfw_zoomSpeedChange',
+		'mfbfw_easing',
+		'mfbfw_easingIn',
+		'mfbfw_easingOut',
+		'mfbfw_easingChange',
+		'mfbfw_imageScale',
+		'mfbfw_enableEscapeButton',
+		'mfbfw_showCloseButton',
+		'mfbfw_centerOnScroll',
+		'mfbfw_hideOnOverlayClick',
+		'mfbfw_hideOnContentClick',
+		'mfbfw_loadAtFooter',
+		'mfbfw_frameWidth',
+		'mfbfw_frameHeight',
+		'mfbfw_callbackOnStart',
+		'mfbfw_callbackOnShow',
+		'mfbfw_callbackOnClose',
+		'mfbfw_galleryType',
+		'mfbfw_customExpression',
+		'mfbfw_nojQuery',
+		'mfbfw_jQnoConflict',
+		'mfbfw_autoApply',
+		'mfbfw_closePosition',
+		'mfbfw_noTextLinks',
+		'mfbfw_uninstall'
+	);
+	
+	foreach ( $deprecated_array as $key ) {
+		delete_option( $key );
+	}
+
+// If no deprecated options present, then get default values
+} else {
+
+	// Get default settings
+	$defaults_array = mfbfw_defaults();
+	
+	// If no options stored, write defaults to database
+	add_option( 'mfbfw', $defaults_array );
+
+}
+
+// Update Version
+update_option( 'mfbfw_active_version', FBFW_VERSION );
+
+?>
