e1e2aa6ba21d3b1ccc575733a31a391b00d6299a
moskis <moskis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-03 15:17:28 +0000

    commit 3.0.2 candidate see https://github.com/moskis/fancybox-for-wordpress for changes
    git-svn-id: https://plugins.svn.wordpress.org/fancybox-for-wordpress/trunk@566930 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fancybox.php
+++ fancybox.php
@@ -3,7 +3,7 @@
 Plugin Name: FancyBox for WordPress
 Plugin URI: http://plugins.josepardilla.com/fancybox-for-wordpress/
 Description: Integrates <a href="http://fancybox.net/">FancyBox</a> by <a href="http://klade.lv/">Janis Skarnelis</a> into WordPress.
-Version: 3.0.1
+Version: 3.0.2
 Author: Jos&eacute; Pardilla
 Author URI: http://josepardilla.com/
 
@@ -15,78 +15,140 @@ Author URI: http://josepardilla.com/
 */
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Define paths with SSL Support on WP3.0+
-/* (http://codex.wordpress.org/Determining_Plugin_and_Content_Directories)
-/*-----------------------------------------------------------------------------------*/
 
-if ( ! function_exists( 'is_ssl' ) ) {
-	function is_ssl() {
-		if ( isset($_SERVER['HTTPS']) ) {
-			if ( 'on' == strtolower($_SERVER['HTTPS']) )
-				return true;
-			if ( '1' == $_SERVER['HTTPS'] )
-				return true;
-		} elseif ( isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT']) ) {
-			return true;
-		}
-		return false;
-	}
+/**
+ * Constants
+ */
+
+define( 'FBFW_VERSION', '3.0.2' );
+define( 'FBFW_PATH', plugin_dir_path(__FILE__) );
+define( 'FBFW_URL', plugin_dir_url(__FILE__) );
+
+
+
+/**
+ * Store default settings in an array
+ */
+
+function mfbfw_defaults() {
+
+	$defaults_array = array(
+
+		// Appearance
+		'border'                => '',
+		'borderColor'           => '#BBBBBB',
+		'showCloseButton'       => 'on',
+		'closeHorPos'           => 'right',
+		'closeVerPos'           => 'top',
+		'paddingColor'          => '#FFFFFF',
+		'padding'               => '10',
+		'overlayShow'           => 'on',
+		'overlayColor'          => '#666666',
+		'overlayOpacity'        => '0.3',
+		'titleShow'             => 'on',
+		'titlePosition'         => 'inside',
+		'titleColor'            => '#333333',
+		'showNavArrows'         => 'on',
+
+		// Animations
+		'zoomOpacity'           => 'on',
+		'zoomSpeedIn'           => '500',
+		'zoomSpeedOut'          => '500',
+		'zoomSpeedChange'       => '300',
+		'transitionIn'          => 'fade',
+		'transitionOut'         => 'fade',
+		'easing'                => '',
+		'easingIn'              => 'easeOutBack',
+		'easingOut'             => 'easeInBack',
+		'easingChange'          => 'easeInOutQuart',
+
+		// Behaviour
+		'imageScale'            => 'on',
+		'centerOnScroll'        => 'on',
+		'hideOnContentClick'    => '',
+		'hideOnOverlayClick'    => 'on',
+		'enableEscapeButton'    => 'on',
+		'cyclic'                => '',
+		'mouseWheel'            => '',
+
+		// Gallery Type
+		'galleryType'           => 'all',
+		'customExpression'      => 'jQuery(thumbnails).addClass("fancybox").attr("rel","fancybox").getTitle();',
+
+		// Other
+		'autoDimensions'        => 'on',
+		'frameWidth'            => '560',
+		'frameHeight'           => '340',
+		'loadAtFooter'          => '',
+		'callbackEnable'        => '',
+		'callbackOnStart'       => 'function() { alert("Start!"); }',
+		'callbackOnCancel'      => 'function() { alert("Cancel!"); }',
+		'callbackOnComplete'    => 'function() { alert("Complete!"); }',
+		'callbackOnCleanup'     => 'function() { alert("CleanUp!"); }',
+		'callbackOnClose'       => 'function() { alert("Close!"); }',
+
+		// Troubleshooting
+		'nojQuery'              => '',
+
+		// Extra Calls
+		'extraCallsEnable'      => '',
+		'extraCalls'            => '',
+
+		// Uninstall
+		'uninstall'             => ''
+
+	);
+
+	return $defaults_array;
 }
 
-if ( version_compare(get_bloginfo('version') , '3.0' , '<') && is_ssl() ) {
-	$wp_content_url = str_replace( 'http://' , 'https://' , get_option('siteurl') );
-} else {
-	$wp_content_url = get_option( 'siteurl' );
+
+
+/**
+ * When plugin is installed, write default settings and update version
+ */
+
+function mfbfw_install() {
+
+	$defaults_array = mfbfw_defaults();
+	add_option( 'mfbfw', $defaults_array );
+	update_option( 'mfbfw_active_version', FBFW_VERSION );
+
 }
-$wp_content_url .= '/wp-content';
-$wp_content_dir = ABSPATH . 'wp-content';
-$wp_plugin_url = $wp_content_url . '/plugins';
-$wp_plugin_dir = $wp_content_dir . '/plugins';
-
-define( 'FBFW_PATH', $wp_plugin_dir . '/fancybox-for-wordpress' );
-define( 'FBFW_URL', $wp_plugin_url . '/fancybox-for-wordpress' );
+register_activation_hook( __FILE__, 'mfbfw_install' );
 
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Main Settings
-/*-----------------------------------------------------------------------------------*/
-
-define( 'FBFW_VERSION', '3.0.1' );
-
-require FBFW_PATH . '/settings.php';
-
-
-
-/*-----------------------------------------------------------------------------------*/
-/* If requested, when plugin is deactivated, remove settings
-/*-----------------------------------------------------------------------------------*/
+/**
+ * If requested, when plugin is deactivated, remove settings
+ */
 
 function mfbfw_uninstall() {
 	$settings = get_option( 'mfbfw' );
-	if ( isset($settings['uninstall']) && $settings['uninstall'] )
+	if ( isset($settings['uninstall']) && $settings['uninstall'] ) {
 		delete_option( 'mfbfw' );
 		delete_option( 'mfbfw_active_version' );
+	}
 }
 register_deactivation_hook( __FILE__, 'mfbfw_uninstall' );
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Here we load FancyBox JS with jQuery and jQuery.easing if necessary
-/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Here we load FancyBox JS with jQuery and jQuery.easing if necessary
+ */
 
 function mfbfw_register_scripts() {
 
 	$settings = get_option( 'mfbfw' );
-	
+
 	// Check if script should be loaded in footer
 	if ( isset($settings['loadAtFooter']) && $settings['loadAtFooter'] ) {
 		$footer = true;
 	} else {
 		$footer = false;
 	}
-	
+
 	// Check if plugin should not call jQuery script (for troubleshooting only)
 	if ( isset($settings['nojQuery']) && $settings['nojQuery'] ) {
 		$jquery = false;
@@ -95,51 +157,46 @@ function mfbfw_register_scripts() {
 	}
 
 	// Register scripts
-	wp_register_script('fancybox', FBFW_URL . '/fancybox/jquery.fancybox.js', $jquery, '1.3.4', $footer ); // Main Fancybox script
-	wp_register_script('jqueryeasing', FBFW_URL . '/js/jquery.easing.1.3.min.js', false, '1.3', $footer ); // Easing animations script
-	wp_register_script('jquerymousewheel', FBFW_URL . '/js/jquery.mousewheel.3.0.4.pack.js', false, '3.0.4', $footer ); // Mouse wheel support script
+	wp_register_script('fancybox', FBFW_URL . 'fancybox/jquery.fancybox.js', $jquery, '1.3.4', $footer ); // Main Fancybox script
+	wp_register_script('jqueryeasing', FBFW_URL . 'js/jquery.easing.1.3.min.js', false, '1.3', $footer ); // Easing animations script
+	wp_register_script('jquerymousewheel', FBFW_URL . 'js/jquery.mousewheel.3.0.4.pack.js', false, '3.0.4', $footer ); // Mouse wheel support script
 
 
 }
 add_action( 'init', 'mfbfw_register_scripts' );
 
-
 function mfbfw_scripts() {
 
 	$settings = get_option( 'mfbfw' );
 
-	if (!is_admin()) { // avoid the scripts from loading on admin panel
-	
-		wp_enqueue_script( 'fancybox' ); // Load fancybox
+	wp_enqueue_script( 'fancybox' ); // Load fancybox
 
-		if ( isset($settings['easing']) && $settings['easing'] ) {
-			wp_enqueue_script( 'jqueryeasing' ); // Load easing javascript file if required
-		}
-		
-		if ( isset($settings['mouseWheel']) && $settings['mouseWheel'] ) {
-			wp_enqueue_script( 'jquerymousewheel' ); // Load mouse wheel javascript file if required
-		}
-		
+	if ( isset($settings['easing']) && $settings['easing'] ) {
+		wp_enqueue_script( 'jqueryeasing' ); // Load easing javascript file if required
+	}
+
+	if ( isset($settings['mouseWheel']) && $settings['mouseWheel'] ) {
+		wp_enqueue_script( 'jquerymousewheel' ); // Load mouse wheel javascript file if required
 	}
 
 }
 add_action( 'wp_enqueue_scripts', 'mfbfw_scripts' ); // Load Scripts
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Link to FancyBox stylesheet and apply some custom styles
-/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Link to FancyBox stylesheet and apply some custom styles
+ */
 
 function mfbfw_styles() {
 
 	$settings = get_option( 'mfbfw' );
-	
-	wp_enqueue_style( 'fancybox', FBFW_URL . '/fancybox/fancybox.css' );
+	wp_enqueue_style( 'fancybox', FBFW_URL . 'fancybox/fancybox.css' );
 
 	?>
 
 	<style type="text/css">
-		div#fancybox-close{<?php echo $settings['closeHorPos']; ?>:-15px;<?php echo $settings['closeVerPos']; ?>:-12px}
+		#fancybox-close{<?php echo $settings['closeHorPos']; ?>:-15px;<?php echo $settings['closeVerPos']; ?>:-15px}
 		<?php if ( isset($settings['paddingColor']) && $settings['paddingColor'] ) { echo "div#fancybox-content{border-color:" . $settings['paddingColor'] . "}\n"; } ?>
 		<?php if ( isset($settings['paddingColor']) && $settings['paddingColor'] && $settings['titlePosition'] == "inside" ) { echo "div#fancybox-title{background-color:" . $settings['paddingColor'] . "}\n"; } ?>
 		div#fancybox-outer{background-color:<?php echo $settings['paddingColor']; if ( isset($settings['border']) && $settings['border'] ) { echo "; border:1px solid " . $settings['borderColor']; } echo "}\n"; ?>
@@ -147,17 +204,18 @@ function mfbfw_styles() {
 	</style>
 
 	<?php
-	
+
 }
 add_action( 'wp_enqueue_scripts', 'mfbfw_styles' );
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Load FancyBox with the settings set
-/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Load FancyBox with the settings set
+ */
 
 function mfbfw_init() {
-	
+
 	$settings = get_option( 'mfbfw' );
 	$version = get_option( 'mfbfw_active_version' );
 
@@ -175,53 +233,43 @@ jQuery.fn.getTitle = function() { // Copy the title of every IMG tag and add it 
 }
 
 // Supported file extensions
-var thumbnails = jQuery("a:has(img)").filter( function() { return /(jpe?g|png|gif|bmp)$/i.test(jQuery(this).attr('href')) });
+var thumbnails = jQuery("a:has(img)").not(".nolightbox").filter( function() { return /\.(jpe?g|png|gif|bmp)$/i.test(jQuery(this).attr('href')) });
 
 <?php if ( $settings['galleryType'] == 'post' ) {
 
 		// Gallery type BY POST and we are on post or page (so only one post or page is visible)
 		if ( is_single() | is_page() ) {
-
 			echo 'thumbnails.addClass("fancybox").attr("rel","fancybox").getTitle();';
-
 		}
 
 		// Gallery type BY POST, but we are neither on post or page, so we make a different rel attribute on each post
 		else {
-
 			echo 'var posts = jQuery(".post");
 
 posts.each(function() {
 	jQuery(this).find(thumbnails).addClass("fancybox").attr("rel","fancybox"+posts.index(this)).getTitle()
 });';
-
 		}
-	
+
 	}
 
 	// Gallery type ALL
 	elseif ( $settings['galleryType'] == 'all' ) {
-
 		echo 'thumbnails.addClass("fancybox").attr("rel","fancybox").getTitle();';
-
 	}
 
 	// Gallery type NONE
 	elseif ( $settings['galleryType'] == 'none' ) {
-
 		echo 'thumbnails.addClass("fancybox").getTitle();';
-
 	}
 
 	// Else, gallery type is custom, so we just print the custom expression
 	else {
-
 		echo $settings['customExpression'];
-
 	}
 
-// Now we call fancybox and apply it on any link with a rel atribute that starts with "fancybox", with the options set on the admin panel ?>
-
+	// Now we call fancybox and apply it on any link with a rel atribute that starts with "fancybox", with the options set on the admin panel
+	?>
 
 jQuery("a.fancybox").fancybox({
 	'cyclic': <?php if ( isset($settings['cyclic']) && $settings['cyclic'] ) { echo "true"; } else { echo "false"; } ?>,
@@ -257,7 +305,7 @@ jQuery("a.fancybox").fancybox({
 } ?>
 
 });
-		
+
 <?php if ( isset($settings['extraCallsEnable']) && $settings['extraCallsEnable'] ) { echo $settings['extraCalls'];  echo "\n"; } ?>
 
 })
@@ -267,14 +315,15 @@ jQuery("a.fancybox").fancybox({
 add_action( 'wp_head', 'mfbfw_init' );
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Load text domain
-/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Load text domain
+ */
 
 function mfbfw_textdomain() {
 
 	if ( function_exists('load_plugin_textdomain') ) {
-		load_plugin_textdomain( 'mfbfw', FBFW_URL . '/languages', 'fancybox-for-wordpress/languages' );
+		load_plugin_textdomain( 'mfbfw', FBFW_URL . 'languages', 'fancybox-for-wordpress/languages' );
 	}
 
 }
@@ -282,9 +331,9 @@ add_action( 'init', 'mfbfw_textdomain' );
 
 
 
-/*-----------------------------------------------------------------------------------*/
-// Register Options
-/*-----------------------------------------------------------------------------------*/
+/**
+ * Register Options
+ */
 
 function mfbfw_admin_options() {
 
@@ -293,7 +342,7 @@ function mfbfw_admin_options() {
 	if ( isset($_GET['page']) && $_GET['page'] == 'fancybox-for-wordpress' ) {
 
 		if ( isset($_REQUEST['action']) && 'update' == $_REQUEST['action'] ) {
-		
+
 			$settings = stripslashes_deep( $_POST['mfbfw'] );
 			$settings = array_map( 'convert_chars', $settings );
 
@@ -318,16 +367,16 @@ add_action( 'admin_init', 'mfbfw_admin_options' );
 
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Admin options page
-/*-----------------------------------------------------------------------------------*/
+/**
+ * Admin options page
+ */
 
 function mfbfw_admin_menu() {
 
-	require FBFW_PATH . '/admin.php';
+	require FBFW_PATH . 'admin.php';
 
 	$mfbfwadmin = add_submenu_page( 'options-general.php', 'Fancybox for WordPress Options', 'Fancybox for WP', 'manage_options', 'fancybox-for-wordpress', 'mfbfw_options_page' );
-	
+
 	add_action( 'admin_print_styles-' . $mfbfwadmin, 'mfbfw_admin_styles' );
 	add_action( 'admin_print_scripts-' . $mfbfwadmin, 'mfbfw_admin_scripts' );
 
@@ -336,25 +385,25 @@ add_action('admin_menu', 'mfbfw_admin_menu');
 
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Load Admin CSS & JS (called in mfbfw_admin_menu())
-/*-----------------------------------------------------------------------------------*/
+/**
+ * Load Admin CSS & JS (called in mfbfw_admin_menu())
+ */
 
 function mfbfw_admin_styles() {
-	wp_enqueue_style( 'fancybox-admin', FBFW_URL . '/css/fancybox-admin.css' ); // Load custom CSS for Admin Page
-	wp_enqueue_style( 'jquery-ui', FBFW_URL . '/css/jquery-ui.css' ); // Load jQuery UI Tabs CSS for Admin Page
+	wp_enqueue_style( 'fancybox-admin', FBFW_URL . 'css/fancybox-admin.css' ); // Load custom CSS for Admin Page
+	wp_enqueue_style( 'jquery-ui', FBFW_URL . 'css/jquery-ui.css' ); // Load jQuery UI Tabs CSS for Admin Page
 }
 
 function mfbfw_admin_scripts() {
 	wp_enqueue_script( 'jquery-ui-tabs', array('jquery-ui-core') ); // Load jQuery UI Tabs JS for Admin Page
-	wp_enqueue_script( 'fancybox-admin', FBFW_URL . '/js/admin.js', array('jquery') ); // Load specific JS for Admin Page
+	wp_enqueue_script( 'fancybox-admin', FBFW_URL . 'js/admin.js', array('jquery') ); // Load specific JS for Admin Page
 }
 
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Settings Button on Plugins Panel
-/*-----------------------------------------------------------------------------------*/
+/**
+ * Settings Button on Plugins Panel
+ */
 
 function mfbfw_plugin_action_links($links, $file) {
 
@@ -370,6 +419,3 @@ function mfbfw_plugin_action_links($links, $file) {
 
 }
 add_filter( 'plugin_action_links', 'mfbfw_plugin_action_links', 10, 2 );
-
-
-?>
