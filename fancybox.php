43eeac0f1a8672391b6bfd0a8fa9c0cd9059514f
moskis <moskis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-05 05:44:12 +0000

    update plugin and author links
    git-svn-id: https://plugins.svn.wordpress.org/fancybox-for-wordpress/trunk@1082866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fancybox.php
+++ fancybox.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: FancyBox for WordPress
-Plugin URI: http://plugins.josepardilla.com/fancybox-for-wordpress/
+Plugin URI: https://wordpress.org/plugins/fancybox-for-wordpress/
 Description: Integrates <a href="http://fancybox.net/">FancyBox</a> by <a href="http://klade.lv/">Janis Skarnelis</a> into WordPress.
-Version: 3.0.4
+Version: 3.0.5
 Author: Jos&eacute; Pardilla
-Author URI: http://josepardilla.com/
+Author URI: http://moskis.net/
 
  * FancyBox is Copyright (c) 2008 - 2010 Janis Skarnelis
  * Dual licensed under the MIT and GPL licenses:
@@ -20,7 +20,7 @@ Author URI: http://josepardilla.com/
  * Constants
  */
 
-define( 'FBFW_VERSION', '3.0.4' );
+define( 'FBFW_VERSION', '3.0.5' );
 define( 'FBFW_PATH', plugin_dir_path(__FILE__) );
 define( 'FBFW_URL', plugin_dir_url(__FILE__) );
 
